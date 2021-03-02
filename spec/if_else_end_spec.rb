e5eaa1225e35fc5174f6002609dc90c27c6742f0
virtual-principle-5400 <mnopprapun@gmail.com>
2021-03-02 18:42:57 +0000

    Done.

--- spec/if_else_end_spec.rb
+++ spec/if_else_end_spec.rb
@@ -1,18 +1,8 @@
-require 'spec_helper'
-describe 'lib/if_else_end.rb' do
-  it 'should output "Even!" if the current second is an even number' do
-    stub_time = Time.at(1582639668)
-    file = Dir.pwd + "/lib/if_else_end.rb"
-    allow(Time).to receive(:new).and_return(stub_time)
-    allow(Time).to receive(:now).and_return(stub_time)
-    expect{ load file }.to output(/Even!/).to_stdout
-  end
-
-  it 'should output "Odd!" if the current second is an odd number' do
-    stub_time = Time.at(1582639667)
-    file = Dir.pwd + "/lib/if_else_end.rb"
-    allow(Time).to receive(:new).and_return(stub_time)
-    allow(Time).to receive(:now).and_return(stub_time)
-    expect{ load file }.to output(/Odd!/).to_stdout
-  end
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
 end
